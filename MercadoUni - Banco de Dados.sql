b6d5f48258fb325af6ddd7902f410f1a906dc22c
Alex Santtos <100032800+AlexSanttos@users.noreply.github.com>
2022-10-29 16:51:27 -0300

    Add files via upload

---
+++ MercadoUni - Banco de Dados.sql
@@ -0,0 +1,298 @@
+CREATE DATABASE  IF NOT EXISTS `bdorcamentogerais` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `bdorcamentogerais`;
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: bdorcamentogerais
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `orçamentofuncionarios`
+--
+
+DROP TABLE IF EXISTS `orçamentofuncionarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orçamentofuncionarios` (
+  `Nome` varchar(50) NOT NULL,
+  `Sálario` int DEFAULT NULL,
+  `Cargo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orçamentofuncionarios`
+--
+
+LOCK TABLES `orçamentofuncionarios` WRITE;
+/*!40000 ALTER TABLE `orçamentofuncionarios` DISABLE KEYS */;
+INSERT INTO `orçamentofuncionarios` VALUES ('Alex Santos',15000,'Diretor'),('Eduardada Micaelly Costa',4000,'Compradora Orçamentária'),('Felipe Santos Porto',2000,'Programador'),('Joao Paulo Dias dos Santos',7000,'DBA'),('Josiel Bispo dos Santos',9000,'Diretor de TI');
+/*!40000 ALTER TABLE `orçamentofuncionarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orçamentoprojeto`
+--
+
+DROP TABLE IF EXISTS `orçamentoprojeto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orçamentoprojeto` (
+  `CodProjeto` int NOT NULL,
+  `NomeProjeto` varchar(45) DEFAULT NULL,
+  `Orçamento` int DEFAULT NULL,
+  PRIMARY KEY (`CodProjeto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orçamentoprojeto`
+--
+
+LOCK TABLES `orçamentoprojeto` WRITE;
+/*!40000 ALTER TABLE `orçamentoprojeto` DISABLE KEYS */;
+INSERT INTO `orçamentoprojeto` VALUES (1058,'Projeto Ambiental',10000),(1486,'Projeto Tratamento do Esgoto',5000),(3524,'FATER',60000),(7456,'Reforma da Andar 03',40000),(8456,'Manutenção do Escritório 124',7000),(9521,'Manutenção Encanamento',11000);
+/*!40000 ALTER TABLE `orçamentoprojeto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orçamentovendas`
+--
+
+DROP TABLE IF EXISTS `orçamentovendas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orçamentovendas` (
+  `IDProduto` int NOT NULL,
+  `NomeProduto` varchar(45) DEFAULT NULL,
+  `RendimentoTotal` int DEFAULT NULL,
+  `QtdVendido` int DEFAULT NULL,
+  `QtdEstoque` int DEFAULT NULL,
+  PRIMARY KEY (`IDProduto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orçamentovendas`
+--
+
+LOCK TABLES `orçamentovendas` WRITE;
+/*!40000 ALTER TABLE `orçamentovendas` DISABLE KEYS */;
+INSERT INTO `orçamentovendas` VALUES (145,'Produto 01',5000,200,400),(258,'Produto 06',6247,90,36),(268,'Produto 02',13000,105,50),(321,'Produto 07',9842,35,80),(369,'Produto 04',65123,589,90),(477,'Produto 05',8426,55,150),(741,'Produto 09',9000,357,159),(753,'Produto 12',4862,22,200),(852,'Produto 08',1500,159,486),(874,'Produto 03',1500,60,10),(951,'Produto 11',2684,862,369),(963,'Produto 10',6358,284,621);
+/*!40000 ALTER TABLE `orçamentovendas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-29 15:42:55
+CREATE DATABASE  IF NOT EXISTS `mercadouni` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `mercadouni`;
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: mercadouni
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `cliente`
+--
+
+DROP TABLE IF EXISTS `cliente`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente` (
+  `Contrato` varchar(100) NOT NULL,
+  `CNPJ` varchar(100) NOT NULL,
+  `Nome` varchar(100) DEFAULT NULL,
+  `Telefone` varchar(100) NOT NULL,
+  PRIMARY KEY (`CNPJ`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente`
+--
+
+LOCK TABLES `cliente` WRITE;
+/*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
+INSERT INTO `cliente` VALUES ('asd','12.311.111/1111-11','asd','+11(11)1 1111-1111');
+/*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-29 15:42:55
+CREATE DATABASE  IF NOT EXISTS `chamadosti` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `chamadosti`;
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: chamadosti
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `chamados`
+--
+
+DROP TABLE IF EXISTS `chamados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chamados` (
+  `Nome` varchar(70) NOT NULL,
+  `Cargo` varchar(45) DEFAULT NULL,
+  `Data` varchar(45) DEFAULT NULL,
+  `Descrição` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`Nome`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `chamados`
+--
+
+LOCK TABLES `chamados` WRITE;
+/*!40000 ALTER TABLE `chamados` DISABLE KEYS */;
+INSERT INTO `chamados` VALUES ('Paulo','Programador','17/10/22','Produtos não funciona');
+/*!40000 ALTER TABLE `chamados` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-29 15:42:55
+CREATE DATABASE  IF NOT EXISTS `sistemabd` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `sistemabd`;
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+--
+-- Host: localhost    Database: sistemabd
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produtos` (
+  `codigoproduto` varchar(100) NOT NULL,
+  `descricaoproduto` varchar(200) NOT NULL,
+  `qtde` varchar(10) NOT NULL,
+  `precounitario` varchar(100) NOT NULL,
+  `precovenda` varchar(100) NOT NULL,
+  PRIMARY KEY (`codigoproduto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produtos`
+--
+
+LOCK TABLES `produtos` WRITE;
+/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
+INSERT INTO `produtos` VALUES (' SO3005691BCO',' Headset sem Fio Sony Pulse 3D? para PS4 e PS5 Branco - 3005691 ',' 10','R$209,00','R$549,00'),(' SO3005768BPO',' Câmera HD Sony Branco e Preto - 3005768',' 25','R$199,00','R$299,00'),('AEMLV93BZAGFTB','iPhone 13 Pro Apple (128GB) Grafite, Tela de 6,1\", 5G e Câmera Tripla de 12MP','33','R$3.899,00','R$5.899,00'),('SOCJ10000320B3','Console Playstation5 Sony 825GB com 01 Controle + Jogo Horizon Forbidden West + Headset sem Fio Sony Pulse 3D Branco','13','R$1.659,00','R$4.659,00');
+/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuario` (
+  `usuario` varchar(30) NOT NULL,
+  `senha` varchar(45) NOT NULL,
+  `nome` varchar(70) DEFAULT NULL,
+  `cargo` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`usuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+INSERT INTO `usuario` VALUES ('alex.dsilva','1234','Alex Santos','Diretor'),('eduarda.mcosta','1234','Eduarda Micaelly Costa','Compradora Orçamentária'),('felipe.sporto','1234','Felipe Santos Porto','Programador'),('joao.pdias','1234','Joao Paulo Dias dos Santos','DBA'),('josiel.bsantos','1234','Josiel Bispo dos Santos','Diretor de TI'),('teste','1234','teste','Programador'),('teste2','1234','testeste','DBA');
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-29 15:42:56
